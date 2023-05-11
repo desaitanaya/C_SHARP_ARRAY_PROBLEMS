091596f7de1818e189aab957769ce47eaca66286
Tanaya <desaitanaya17@gmail.com>
2023-05-11 23:42:47 +0530

    [Tanaya] Add : C# Program to find out the smallest gap between the numbers in array.

---
+++ ArraySmallestGap/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace ArraySmallestGap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size;
+
+            Console.WriteLine("Enter the size of array :");
+            size = int.Parse(Console.ReadLine());
+
+            //Array declaration
+            int[] array = new int[size];
+
+            Console.WriteLine("Enter the array elements : ");
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            int temp = 0;
+            //Sorting the elements in Ascending order
+            for (int i = 0; i < size - 1; i++)
+            {
+                for (int j = i + 1; j < size; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+
+            }
+
+            //Finding out the minimum gap between the elements in array
+            int min_gap = int.MaxValue;
+            for(int i = 0; i < size; i++)
+            {
+                for(int j = i+1; j < size; j++)
+                {
+                    if (array[j] - array[i] < min_gap)
+                    {
+                        min_gap = array[j] - array[i];
+                    }
+                }
+                
+            }
+
+            Console.WriteLine("Minimum gap between the elements in array is : " + min_gap);
+        }
+    }
+}
