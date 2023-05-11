5152ca56b5bc1fe306a69f08402943cdae8511f3
Tanaya <desaitanaya17@gmail.com>
2023-05-11 23:12:51 +0530

    [Tanaya] Add : C# Program to find the missing element in given array.

---
+++ ArrayMissingElement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ArrayMissingELement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size, total_sum = 0;
+
+            Console.WriteLine("Enter the size of array :");
+            size = int.Parse(Console.ReadLine());
+
+            //Array declaration
+            int[] array = new int[size];
+
+            Console.WriteLine("Enter the array elements : ");
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            //Calculating the sum of natural numbers in array
+            total_sum = (size + 1) * (size + 2) / 2;
+            for(int i=0; i < size;i++)
+            {
+                //Subtracting the total sum by the element present in array
+                total_sum -= array[i];
+            }
+
+            Console.WriteLine("Missing element in the array is : " + total_sum);
+        }
+    }
+}
