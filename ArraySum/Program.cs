9983242dcb5d138c3c62cd96f48567eb732a8240
Tanaya <desaitanaya17@gmail.com>
2023-05-11 19:56:58 +0530

    [Tanaya] Add : C# Program to display the sum of all array elements.

---
+++ ArraySum/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ArraySum
+{
+   class ArraySum
+    {
+        static void Main(string[] args)
+        {
+            int size, sum=0;
+            Console.WriteLine("Enter the size of array :");
+            size = int.Parse(Console.ReadLine());
+
+            int[] array = new int[size];
+
+            //Taking input from the user
+            Console.WriteLine("Enter the array elements : ");
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            //Displaying the sum of elements in array
+            Console.WriteLine("Sum of elements in array : ");
+            for (int i = 0; i<size; i++)
+            {
+                sum = sum + array[i];
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
