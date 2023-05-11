41198dcce0699d6f61359c44df93e89eb6d166a5
Tanaya <desaitanaya17@gmail.com>
2023-05-11 20:41:18 +0530

    [Tanaya] Add : C# Program to display the odd values from array.

---
+++ ArrayOddValues/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ArrayOddValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size, j = 0, k = 0;
+            Console.WriteLine("Enter the size of array :");
+            size = int.Parse(Console.ReadLine());
+
+            //Declaring array
+            int[] array = new int[size];
+            
+
+            //Taking input from the user
+            Console.WriteLine("Enter the array elements : ");
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Odd elements in the array are : ");
+            for (int i = 0; i < size; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+
+            
+
+  
+
+        }
+    }
+}
