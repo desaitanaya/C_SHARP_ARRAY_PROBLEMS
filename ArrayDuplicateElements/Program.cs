1af3ade3bf7b871807788f8427b8b8fb58000312
Tanaya <desaitanaya17@gmail.com>
2023-05-11 20:04:03 +0530

    [Tanaya] Add : C# Program to count the Duplicate elements in array.

---
+++ ArrayDuplicateElements/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ArrayDuplicateElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size, count = 0;
+
+            Console.WriteLine("Enter the size of array :");
+            size = int.Parse(Console.ReadLine());
+
+            //Array declaration
+            int[] array = new int[size];
+
+            Console.WriteLine("Enter the array elements : ");
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            
+            //Finding out the duplicate elements in array
+            for(int i=0; i < size; i++)
+            {
+                for(int j=i+1; j <size; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        count = count + 1;
+                    }
+                }
+               
+            }
+
+            Console.WriteLine("Count of Duplicate elements in array : " + count);
+        }
+    }
+}
