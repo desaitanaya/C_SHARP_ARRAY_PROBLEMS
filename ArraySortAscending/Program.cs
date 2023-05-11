d121e7ab37e1ea3ce048fbe98def3c127a571774
Tanaya <desaitanaya17@gmail.com>
2023-05-11 20:30:29 +0530

    [Tanaya] Add : C# Program to sort the elements of array in Ascending order.

---
+++ ArraySortAscending/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ArraySortAscending
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size;
+
+            Console.WriteLine("Enter the size of array :");
+            size = int.Parse(Console.ReadLine());
+
+            //Array declaration
+            int[] array = new int[size];
+
+            Console.WriteLine("Enter the array elements : ");
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            int temp = 0;
+            for (int i = 0; i < size-1; i++)
+            {
+                for(int j=i+1; j<size; j++)
+                {
+                    //Sorting the elements in ascending order
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+                
+            }
+
+            Console.WriteLine("Array elements in Ascending order : ");
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+
+        }
+    }
+}
