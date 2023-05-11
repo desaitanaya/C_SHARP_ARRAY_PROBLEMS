0cd21beb92bbf693ce0a498ee1a69ed317fbbc93
Tanaya <desaitanaya17@gmail.com>
2023-05-11 19:51:03 +0530

    [Tanaya] Add : C# Program that stores elements in Array and Displays the Array elements.

---
+++ ArrayProblems/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ArrayProblems
+{
+    class PrintArrayElements
+    {
+        static void Main(string[] args)
+        {
+            //Declaring an array
+            int[] array = new int[5];
+
+            Console.WriteLine("Enter the array elements : ");
+            for(int i=0; i<10; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            
+            //Displaying the array elements
+            Console.WriteLine("Array elements are : ");
+            for(int i=0; i < 10; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
