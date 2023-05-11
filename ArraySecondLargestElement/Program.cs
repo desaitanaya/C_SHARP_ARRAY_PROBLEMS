e3b59097d494fa66541b05c101edd0ea62d4d25d
Tanaya <desaitanaya17@gmail.com>
2023-05-11 20:35:27 +0530

    [Tanaya] Add : C# Program to find the Second Largest element in array.

---
+++ ArraySecondLargestElement/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ArraySecondLargestElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size, i = 0;
+
+            Console.WriteLine("Enter the size of array :");
+            size = int.Parse(Console.ReadLine());
+
+            //Array declaration
+            int[] array = new int[size];
+
+            Console.WriteLine("Enter the array elements : ");
+            for (i = 0; i < size; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            int temp = 0;
+            for (i = 0; i < size - 1; i++)
+            {
+                for (int j = i + 1; j < size; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+
+            }
+            Console.WriteLine("Second largest element in array is : " + array[size-2]);
+
+        }
+    }
+}
