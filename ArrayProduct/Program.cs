09a31a141721bca9fcabb2e0bdf2059142ca47de
Tanaya <desaitanaya17@gmail.com>
2023-05-11 20:48:01 +0530

    [Tanaya] Add : C# Program to find two numbers in array whose product is equal to given number.

---
+++ ArrayProduct/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ArrayProduct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size, num;
+            Console.WriteLine("Enter the size of array :");
+            size = int.Parse(Console.ReadLine());
+
+            //Declaring array
+            int[] array = new int[size];
+            
+            //Taking input from the user
+            Console.WriteLine("Enter the array elements : ");
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter the number : ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Numbers whose product is equal to given number : ");
+            for(int i = 0; i < size; i++)
+            {
+                for(int j = i+1; j < size; j++)
+                {
+                    if (array[i] * array[j] == num)
+                    {
+                        Console.WriteLine(array[i]);
+                        Console.WriteLine(array[j]);
+                    }
+                }
+            }
+
+        }
+    }
+}
