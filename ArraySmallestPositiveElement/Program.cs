b49b25bb055216d9ef96eada346493e866a388c1
Tanaya <desaitanaya17@gmail.com>
2023-05-12 09:05:51 +0530

    [Tanaya] Add : C# Program to find the Smallest Positive integer that is not present in array.

---
+++ ArraySmallestPositiveElement/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ArraySmallestPositiveElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size;
+
+            Console.WriteLine("Enter the size of array :");
+            size = int.Parse(Console.ReadLine());
+
+            //Array declaration
+            int[] array = new int[size];
+
+            Console.WriteLine("Enter the array elements : ");
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            int temp = 0;
+            //Sorting the array elements in ascending order
+            for (int i = 0; i < size - 1; i++)
+            {
+                for (int j = i + 1; j < size; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+
+            }
+            int num = 1;
+            for(int i= 0; i < size; i++)
+            {
+                /*Will check if the number is present in array or not and if
+                 * not then it will return that number */
+                if (array[i] == num)
+                {
+                    num++;
+                }
+            }
+            Console.WriteLine("Smallest positive missing element in array is : " + num);
+        }
+    }
+}
